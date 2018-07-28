d2920ab1d8b7354604b99f2e873be7744433974d
ruslan <absaruslan90@mail.ru>
2018-07-28 15:20:56 +0300

    Подключение AOS библеотеки

--- resource/js/materialize.js
+++ resource/js/materialize.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
     $('.tabs').tabs();
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+    AOS.init({
+        once: true,
+        duration: 400,
+    });
 });
