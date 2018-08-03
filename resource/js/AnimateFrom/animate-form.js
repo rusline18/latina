4bba5070a4953c2a7abf5c1a06323b6bd05ab990
ruslan <absaruslan90@mail.ru>
2018-08-03 13:42:16 +0300

    Создание своего плагина AnimatedForm

---
+++ resource/js/AnimateFrom/animate-form.js
@@ -0,0 +1,12 @@
+(function( $ ){
+    $.fn.animatedForm = function () {
+        return this.html(`<div class="sa">
+<div class="sa-success">
+<div class="sa-success-tip"></div>
+<div class="sa-success-long"></div>
+<div class="sa-success-placeholder"></div>
+<div class="sa-success-fix"></div>
+</div>
+</div>`)
+    }
+})( JQuery );
--- resource/js/form.js
+++ resource/js/form.js
@@ -3,10 +3,34 @@ $(document).ready(function(){
         e.preventDefault();
         $.post({
             url: window.location.origin+'/form.php',
-            data: $(this).serialize()
+            data: $(this).serialize(),
+            beforeSend: function () {
+                $('.focus_form-button').html(`<div class="preloader-wrapper small active">
+                <div class="spinner-layer spinner-default-only">
+                  <div class="circle-clipper left">
+                    <div class="circle"></div>
+                  </div><div class="gap-patch">
+                    <div class="circle"></div>
+                  </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                  </div>
+                </div>
+              </div>`);
+            }
         })
             .done(request => {
                 let res = JSON.parse(request);
+                if (res.error === true){
+                    setTimeout(function () {
+                        $('.focus_form-message').html(`<span class="error">${res.message}</span>`);
+                        $('.focus_form-button').html(`Записаться`);
+                    }, 500);
+                } else {
+                    setTimeout(function () {
+                        $('.focus_form-message').animatedForm();
+                        // $('.focus_form-button').html(`Записаться`);
+                    }, 500);
+                }
                 console.log(res);
             })
             .fail(err => console.error(err))
