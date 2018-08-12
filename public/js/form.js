398b25afad6e2826d84bf9f1f246e34f8aaa445b
ruslan <absaruslan90@mail.ru>
2018-08-12 19:21:49 +0300

    Рефакторинг кода

---
+++ public/js/form.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+    form('#focus_form_recording', '.focus_form-button', '.focus_form-message', '.focus-form');
+    form('#modal-form', '.modal_form-button', '.modal_form-message', '.modal-content');
+
+    function form(form, button, message, content){
+        $(form).submit(function (e) {
+            e.preventDefault();
+            $.post({
+                url: window.location.origin+'/form.php',
+                data: $(this).serialize(),
+                beforeSend: function () {
+                    $(button).html(`<div class="preloader-wrapper small active">
+                <div class="spinner-layer spinner-default-only">
+                  <div class="circle-clipper left">
+                    <div class="circle"></div>
+                  </div><div class="gap-patch">
+                    <div class="circle"></div>
+                  </div><div class="circle-clipper right">
+                    <div class="circle"></div>
+                  </div>
+                </div>
+              </div>`);
+                }
+            })
+                .done(request => {
+                    let res = JSON.parse(request);
+                    if (res.error === true){
+                        setTimeout(function () {
+                            $(message).html(`<span class="error">${res.message}</span>`);
+                            $(button).html(`Записаться`);
+                        }, 500);
+                    } else {
+                        setTimeout(function () {
+                            $(content).animatedForm({title: res.message});
+                        }, 500);
+                    }
+                })
+                .fail(() => {
+                    $(content).animatedForm({
+                        title: 'Произошла ошибка! Попробуйте позже',
+                        type: 'error'
+                    });
+                })
+        })
+    }
+});
