4dab540198d0d652db8f7b9fd92f05618b7f591f
ruslan <absaruslan90@mail.ru>
2018-08-26 13:52:58 +0300

    Связь один к одному для направление

--- admin/app/Lid.php
+++ admin/app/Lid.php
@@ -4,9 +4,19 @@ namespace Latina;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Модель заявок с лендинга
+ * Class Lid
+ * @package Latina
+ */
 class Lid extends Model
 {
     protected $table = 'lid';
     protected  $dateFormat = 'U';
     protected $fillable = ['name', 'phone', 'direction_id', 'type'];
+
+    public function direction()
+    {
+        return $this->hasOne('Latina\Direction');
+    }
 }
